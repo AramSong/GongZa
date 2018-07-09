51336ebd5409dc393498a523db655f70532d8aa1
Jang Yaewon <rainsimple@naver.com>
2018-07-09 09:09:16 +0000

    템플릿 디테일(show)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,14 @@
 require 'csv'
 
-csv_text = File.read(Rails.root.join('lib', 'seeds', 'food_name.csv'))
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'seoul_dong.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'UTF-8')
 csv.each do |row|
-  t = Food.new
-  t.food_name = row['음식']
+  t = Road.new
   
+  d = District.find_by_district_name(row['자치구'])
+  t.district_id = d.id
+  t.road_name = row['동']
+  t.state_id = 1
   t.save
   
 end
